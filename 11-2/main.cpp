541c639c8fed0580614c1760a7bc378fb4d491c4
kindofman <43539680+kindofman@users.noreply.github.com>
2019-12-14 12:24:51 +0300

    Update main.cpp

--- 11-2/main.cpp
+++ 11-2/main.cpp
@@ -23,18 +23,16 @@ using std::cout;
 class Graph {
 public:
     Graph(int v): v(v) { adj.assign(v, {}); };
-    void BFS( vector<vector <int> > adj, int src, int dist[], int* paths, int n );
-    int find_shortest_paths( int s, int n, int w );
+    void BFS( int src, vector<int>& dist, vector<int>& paths, int n ) const;
+    int find_shortest_paths( int s, int n, int w ) const;
     void add_edge( int u, int v );
 private:
     const int v;
     vector<vector <int> > adj;
 };
 
-void Graph::BFS( vector<vector <int> > adj, int src, int dist[], int* paths, int n ) {
-    bool* visited = new bool[n];
-    for (int i = 0; i < n; i++)
-        visited[i] = false;
+void Graph::BFS( int src, vector<int>& dist, vector<int>& paths, int n ) const {
+    vector<bool> visited( n, false );
     dist[src] = 0;
     paths[src] = 1;
   
@@ -48,6 +46,49 @@ void Graph::BFS( vector<vector <int> > adj, int src, int dist[], int* paths, int
         for (auto x : adj[curr]) {
             if (visited[x] == false) {
                 q.push(x);
+                visited[x] = true;
+            }
+            if (dist[x] > dist[curr] + 1) {
+                dist[x] = dist[curr] + 1;
+                paths[x] = paths[curr];
+            }
+            else if (dist[x] == dist[curr] + 1)
+                paths[x] += paths[curr];
+        }
+    }
+}
+  
+int Graph::find_shortest_paths( int s, int n, int w ) const {
+    vector<int> dist( n, INT_MAX );
+    vector<int> paths( n, 0 );
+    BFS( s, dist, paths, n );
+    int result = paths[w];
+    return result;
+}
+  
+void Graph::add_edge( int u, int v ) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+int main() {
+    int v_, n;
+    std::cin >> v_ >> n;
+    Graph g(v_);
+    for ( int i = 0; i < n; ++i ) {
+        int u, v;
+        std::cin >> u >> v;
+        g.add_edge( u, v );
+    }
+    int v, w;
+    std::cin >> v >> w;
+    int result = g.find_shortest_paths( v, v_, w );
+      cout << result << std::endl;
+  
+    return 0;
+}
+
+
                 visited[x] = true;
             }
             if (dist[x] > dist[curr] + 1) {
